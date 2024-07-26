11253595e21cced0eab6f05efedc40e86e217193
Aishworya Shrivastav <103126410+aishwory@users.noreply.github.com>
2024-07-26 11:50:35 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//select the button and paragraph of elements//
+const button =document.getElementById('myButton');
+const button =document.getElementById('message');
+//displaying functions//
+function showMessage(){
+    message.textContent ='Hello, this is a message'
+    //adding event listener
+    button.addEventListener('click'.showMessage);
